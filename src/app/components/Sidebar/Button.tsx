650f2f3feeeb7bab7df36659d028b1b995a38119
Yago Russo <yagrrusso@gmail.com>
2024-02-08 18:35:46 -0300

    chore: sidebar button hover effect

--- src/app/components/Sidebar/Button.tsx
+++ src/app/components/Sidebar/Button.tsx
@@ -21,9 +21,9 @@ const SidebarButton: FC<SidebarButtonProps> = ({
   return (
     <button
       className={clsx(
-        'flex items-center gap-2 rounded-r-lg px-4 py-2 font-medium transition-all hover:text-sky-400',
+        'flex items-center gap-2 rounded-r-lg border-sky-400 px-4 py-2 font-medium transition-all hover:border-l-4 hover:text-sky-400',
         path === pathname?.replace(/\/$/, '') &&
-          'border-l-4 border-sky-400 bg-sky-400 bg-opacity-15 font-semibold text-sky-400',
+          'border-l-4 bg-sky-400 bg-opacity-15 font-semibold text-sky-400',
         className,
       )}
       {...rest}

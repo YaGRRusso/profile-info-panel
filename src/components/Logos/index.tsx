a1513b1e091ee117a80411427835b5013868ac1e
Yago Russo <yagrrusso@gmail.com>
2024-02-28 23:21:27 -0300

    fix: static images

--- src/components/Logos/index.tsx
+++ src/components/Logos/index.tsx
@@ -10,21 +10,21 @@ const Logos: FC<LogosProps> = ({ ...rest }) => {
       {...rest}
     >
       <Image
-        src="yr-branco.svg"
+        src="/yr-branco.svg"
         alt="logo one"
         width={120}
         height={120}
         className="-mr-8 hidden dark:block"
       />
       <Image
-        src="yr-preto.svg"
+        src="/yr-preto.svg"
         alt="logo two"
         width={120}
         height={120}
         className="-mr-8 block dark:hidden"
       />
       <Image
-        src="yr-cor.svg"
+        src="/yr-cor.svg"
         alt="logo three"
         width={120}
         height={120}

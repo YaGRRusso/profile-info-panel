30e9f5858da1e9b877d1b820154990b46458406d
Yago Russo <yagrrusso@gmail.com>
2024-02-28 23:15:31 -0300

    chore: index redirection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
+import options from '@/common/auth'
+
 import { redirect } from 'next/navigation'
+import { getServerSession } from 'next-auth'
 
 export default async function Home() {
-  redirect('/signin')
+  const session = await getServerSession(options)
+  session ? redirect('/home') : redirect('/signin')
 }

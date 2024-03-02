88fa2e94c7ea449543c2e625b488026b3977b7a7
Yago Russo <yagrrusso@gmail.com>
2024-03-02 18:21:46 -0300

    chore: sign up form redesign

--- src/app/[locale]/(authenticated)/signup/components/SignUpForm/index.tsx
+++ src/app/[locale]/(authenticated)/signup/components/SignUpForm/index.tsx
@@ -1,6 +1,8 @@
 'use client'
 
 import { Button, Emphasis, TextInput, Form } from '@/components'
+import CheckboxInput from '@/components/Inputs/Checkbox'
+import TextAreaInput from '@/components/Inputs/TextArea'
 import { mask } from '@/helpers/mask'
 
 import { zodResolver } from '@hookform/resolvers/zod'
@@ -9,7 +11,6 @@ import {
   Envelope,
   Eye,
   IdentificationCard,
-  Image,
   List,
   Package,
   Phone,
@@ -18,24 +19,30 @@ import {
 } from '@phosphor-icons/react'
 import Link from 'next/link'
 import { useTranslations } from 'next-intl'
-import { FC, FormHTMLAttributes, useCallback } from 'react'
+import { FC, FormHTMLAttributes, useCallback, useState } from 'react'
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
 
-const formSchema = z.object({
-  name: z.string().min(1),
-  birth: z.string().min(1).datetime(),
-  description: z.string().min(1),
-  email: z.string().min(1).email(),
-  password: z.string().min(1),
-  nickname: z.string().min(1),
-  phone: z.string().min(1),
-  picture: z.string().min(1),
-  postal: z.string().min(1),
-  presentation: z.string().min(1),
-  address: z.string().min(1),
-  title: z.string().min(1),
-})
+const formSchema = z
+  .object({
+    name: z.string().min(1),
+    birth: z.string().min(1),
+    description: z.string().min(1),
+    email: z.string().min(1).email(),
+    password: z.string().min(1),
+    nickname: z.string().min(1),
+    phone: z.string().min(1),
+    picture: z.string().optional(),
+    postal: z.string().min(1),
+    presentation: z.string().optional(),
+    address: z.string().min(1),
+    title: z.string().min(1),
+    passwordConfirm: z.string().min(1).optional(),
+  })
+  .refine((data) => data.password === data.passwordConfirm, {
+    message: 'sameValue',
+    path: ['passwordConfirm'],
+  })
 
 type FormSchemaProps = z.infer<typeof formSchema>
 
@@ -44,6 +51,7 @@ export interface SignUpFormProps extends FormHTMLAttributes<HTMLFormElement> {
 }
 
 const SignUpForm: FC<SignUpFormProps> = ({ defaultValues, ...rest }) => {
+  const [isShowingPassword, setIsShowingPassword] = useState(false)
   const tSignUp = useTranslations('signUp')
   const tForm = useTranslations('form')
 
@@ -56,12 +64,13 @@ const SignUpForm: FC<SignUpFormProps> = ({ defaultValues, ...rest }) => {
     resolver: zodResolver(formSchema),
     defaultValues: {
       address: '',
-      birth: '1970-12-31T12:00:00.000Z',
+      birth: '',
       description: '',
       email: '',
       name: '',
       nickname: '',
       password: '',
+      passwordConfirm: '',
       phone: '',
       picture: 'https://picsum.photos/60',
       postal: '',
@@ -72,33 +81,22 @@ const SignUpForm: FC<SignUpFormProps> = ({ defaultValues, ...rest }) => {
   })
 
   const onSubmit = useCallback(async (data: FormSchemaProps) => {
+    delete data.passwordConfirm
     console.log(data)
   }, [])
 
   return (
     <Form.Root onSubmit={handleSubmit(onSubmit)} {...rest}>
-      <div className="grid grid-cols-[repeat(auto-fit,_minmax(240px,1fr))] gap-x-6 gap-y-4">
-        <Form.Group title="Name" error={errors.name?.message}>
+      <div className="grid grid-cols-[repeat(auto-fit,_minmax(180px,1fr))] gap-x-6 gap-y-4">
+        <Form.Group
+          title="Name"
+          error={errors.name?.message}
+          className="col-span-full"
+        >
           <TextInput
             icon={<User />}
             onChange={(ev) => setValue('name', ev.target.value)}
             value={watch('name')}
-            placeholder={tForm('writeHere')}
-          />
-        </Form.Group>
-        <Form.Group title="Description" error={errors.description?.message}>
-          <TextInput
-            icon={<List />}
-            onChange={(ev) => setValue('description', ev.target.value)}
-            value={watch('description')}
-            placeholder={tForm('writeHere')}
-          />
-        </Form.Group>
-        <Form.Group title="Birth" error={errors.birth?.message}>
-          <TextInput
-            icon={<Cake />}
-            onChange={(ev) => setValue('birth', ev.target.value)}
-            value={watch('birth')}
             placeholder={tForm('writeHere')}
           />
         </Form.Group>
@@ -110,20 +108,11 @@ const SignUpForm: FC<SignUpFormProps> = ({ defaultValues, ...rest }) => {
             placeholder={tForm('writeHere')}
           />
         </Form.Group>
-        <Form.Group title="Email" error={errors.email?.message}>
+        <Form.Group title="Title" error={errors.title?.message}>
           <TextInput
-            icon={<Envelope />}
-            onChange={(ev) => setValue('email', ev.target.value)}
-            value={watch('email')}
-            placeholder={tForm('writeHere')}
-          />
-        </Form.Group>
-        <Form.Group title="Password" error={errors.password?.message}>
-          <TextInput
-            icon={<Eye onClick={() => console.log('oi')} />}
-            onChange={(ev) => setValue('password', ev.target.value)}
-            value={watch('password')}
-            type="password"
+            icon={<IdentificationCard />}
+            onChange={(ev) => setValue('title', ev.target.value)}
+            value={watch('title')}
             placeholder={tForm('writeHere')}
           />
         </Form.Group>
@@ -140,12 +129,72 @@ const SignUpForm: FC<SignUpFormProps> = ({ defaultValues, ...rest }) => {
             }
           />
         </Form.Group>
-        <Form.Group title="Picture" error={errors.picture?.message}>
+        <Form.Group title="Birth" error={errors.birth?.message}>
           <TextInput
-            // eslint-disable-next-line jsx-a11y/alt-text
-            icon={<Image />}
-            onChange={(ev) => setValue('picture', ev.target.value)}
-            value={watch('picture')}
+            icon={<Cake />}
+            onChange={(ev) => setValue('birth', ev.target.value)}
+            value={watch('birth')}
+            placeholder={tForm('writeHere')}
+            type="date"
+          />
+        </Form.Group>
+        <Form.Group
+          title="Description"
+          error={errors.description?.message}
+          className="col-span-full"
+        >
+          <TextAreaInput
+            icon={<List />}
+            onChange={(ev) => setValue('description', ev.target.value)}
+            value={watch('description')}
+            placeholder={tForm('writeHere')}
+          />
+        </Form.Group>
+        <Form.Group
+          title="Email"
+          error={errors.email?.message}
+          className="col-span-full"
+        >
+          <TextInput
+            icon={<Envelope />}
+            onChange={(ev) => setValue('email', ev.target.value)}
+            value={watch('email')}
+            placeholder={tForm('writeHere')}
+          />
+        </Form.Group>
+        <Form.Group title="Password" error={errors.password?.message}>
+          <TextInput
+            icon={<Eye />}
+            onChange={(ev) => setValue('password', ev.target.value)}
+            value={watch('password')}
+            type={isShowingPassword ? 'text' : 'password'}
+            placeholder={tForm('writeHere')}
+          />
+        </Form.Group>
+        <Form.Group
+          title="Password Confirm"
+          error={errors.passwordConfirm?.message}
+        >
+          <TextInput
+            icon={<Eye />}
+            onChange={(ev) => setValue('passwordConfirm', ev.target.value)}
+            value={watch('passwordConfirm')}
+            type={isShowingPassword ? 'text' : 'password'}
+            placeholder={tForm('writeHere')}
+          />
+        </Form.Group>
+        <Form.Group className="col-span-full">
+          <CheckboxInput
+            onChange={() => setIsShowingPassword(!isShowingPassword)}
+            checked={isShowingPassword}
+            placeholder={tForm('showPassword')}
+          />
+        </Form.Group>
+        <Form.Group title="Address" error={errors.address?.message}>
+          <TextInput
+            icon={<PushPin />}
+            onChange={(ev) => setValue('address', ev.target.value)}
+            value={watch('address')}
             placeholder={tForm('writeHere')}
           />
         </Form.Group>
@@ -160,30 +209,6 @@ const SignUpForm: FC<SignUpFormProps> = ({ defaultValues, ...rest }) => {
                 mask('00000-000', ev.target.value).unmaskedValue,
               )
             }
-          />
-        </Form.Group>
-        <Form.Group title="Presentation" error={errors.presentation?.message}>
-          <TextInput
-            icon={<List />}
-            onChange={(ev) => setValue('presentation', ev.target.value)}
-            value={watch('presentation')}
-            placeholder={tForm('writeHere')}
-          />
-        </Form.Group>
-        <Form.Group title="Address" error={errors.address?.message}>
-          <TextInput
-            icon={<PushPin />}
-            onChange={(ev) => setValue('address', ev.target.value)}
-            value={watch('address')}
-            placeholder={tForm('writeHere')}
-          />
-        </Form.Group>
-        <Form.Group title="Title" error={errors.title?.message}>
-          <TextInput
-            icon={<IdentificationCard />}
-            onChange={(ev) => setValue('title', ev.target.value)}
-            value={watch('title')}
-            placeholder={tForm('writeHere')}
           />
         </Form.Group>
       </div>

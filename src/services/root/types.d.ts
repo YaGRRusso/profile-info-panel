d49e1f0b90d74b4a92d1b2a71acf53e42122c37c
Yago Russo <yagrrusso@gmail.com>
2024-03-20 19:39:14 -0300

    chore: removed services

--- src/services/root/types.d.ts
+++
@@ -1,2 +0,0 @@
-export type GetHelloOutput = Record<string, unknown>
-export type GetHelloProps = () => Promise<GetHelloOutput>
--- src/services/skills/types.d.ts
+++
@@ -1,8 +0,0 @@
-export type GetSkillOutput = {
-  category: string
-  createdAt: Date
-  id: string
-  name: string
-  updatedAt: Date
-}
-export type GetSkillsProps = () => Promise<GetSkillOutput[]>

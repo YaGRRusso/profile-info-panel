75b21e5263d6f236f6552d45b7c0cbcf04d2d5cf
Yago Russo <yagrrusso@gmail.com>
2024-03-21 23:17:56 -0300

    fix: default timezone on date tests

--- src/helpers/date.test.ts
+++ src/helpers/date.test.ts
@@ -5,6 +5,7 @@ describe('formatDate', () => {
 
   it('should return formatted date with year, month, day, hour and minute', async () => {
     const res = formatDate(date, {
+      timeZone: 'America/Sao_Paulo',
       weekday: 'long',
       year: 'numeric',
       month: '2-digit',
@@ -17,6 +18,7 @@ describe('formatDate', () => {
 
   it('should return formatted date with year, month, day', async () => {
     const res = formatDate(date, {
+      timeZone: 'America/Sao_Paulo',
       year: 'numeric',
       month: '2-digit',
       day: '2-digit',
@@ -25,12 +27,19 @@ describe('formatDate', () => {
   })
 
   it('should return formatted date with hour and minute', async () => {
-    const res = formatDate(date, { hour: '2-digit', minute: '2-digit' })
+    const res = formatDate(date, {
+      timeZone: 'America/Sao_Paulo',
+      hour: '2-digit',
+      minute: '2-digit',
+    })
     expect(res).toEqual('20:59')
   })
 
   it('should return undefined', async () => {
-    const res = formatDate(undefined, { day: '2-digit' })
+    const res = formatDate(undefined, {
+      timeZone: 'America/Sao_Paulo',
+      weekday: 'long',
+    })
     expect(res).toEqual(undefined)
   })
 })

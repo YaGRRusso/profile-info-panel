e3a36993f51bdb1070e1d7943597171825f773bf
Yago Russo <yagrrusso@gmail.com>
2024-03-21 21:57:48 -0300

    feat: date helper

---
+++ src/helpers/date.test.ts
@@ -0,0 +1,31 @@
+import { formatDate } from './date'
+
+describe('formatDate', () => {
+  const date = '2020-12-29T23:59:59.100Z'
+
+  it('should return formatted date with year, month, day, hour and minute', async () => {
+    const res = formatDate(date, {
+      weekday: 'long',
+      year: 'numeric',
+      month: '2-digit',
+      day: '2-digit',
+      hour: '2-digit',
+      minute: '2-digit',
+    })
+    expect(res).toEqual('terça-feira, 29/12/2020, 20:59')
+  })
+
+  it('should return formatted date with year, month, day', async () => {
+    const res = formatDate(date, {
+      year: 'numeric',
+      month: '2-digit',
+      day: '2-digit',
+    })
+    expect(res).toEqual('29/12/2020')
+  })
+
+  it('should return formatted date with hour and minute', async () => {
+    const res = formatDate(date, { hour: '2-digit', minute: '2-digit' })
+    expect(res).toEqual('20:59')
+  })
+})

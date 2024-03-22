f7b556d66cb5402257f96b05135c01e79d6c0c8d
Yago Russo <yagrrusso@gmail.com>
2024-03-21 22:27:34 -0300

    feat: projects table

---
+++ src/app/[locale]/(authenticated)/projects/components/Table.tsx
@@ -0,0 +1,74 @@
+'use client'
+
+import { IconText, Table, TableRootProps } from '@/components'
+import { formatDate } from '@/helpers/date'
+import { useProjects } from '@/sdk'
+
+import { CircleNotch } from '@phosphor-icons/react'
+import { useQuery } from '@tanstack/react-query'
+import Link from 'next/link'
+import { forwardRef } from 'react'
+
+export interface ProjectsTableProps extends TableRootProps {}
+
+const ProjectsTable = forwardRef<HTMLTableElement, ProjectsTableProps>(
+  ({ ...rest }, ref) => {
+    const projects = useProjects()
+
+    const { data, isFetching } = useQuery({
+      queryKey: ['projects'],
+      queryFn: () => projects.projectsControllerFindAll(),
+    })
+
+    return (
+      <Table.Root ref={ref} {...rest}>
+        {!data && (
+          <Table.Caption className="py-8">
+            <IconText
+              icon={isFetching && <CircleNotch className="animate-spin" />}
+              text={isFetching ? 'Loading...' : 'Empty'}
+            />
+          </Table.Caption>
+        )}
+        <Table.Header>
+          <Table.Row>
+            <Table.Head>Name</Table.Head>
+            <Table.Head>Description</Table.Head>
+            <Table.Head>Image</Table.Head>
+            <Table.Head>Link</Table.Head>
+            <Table.Head className="text-right">Last Update</Table.Head>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {data?.data.map((project) => (
+            <Table.Row key={project.id}>
+              <Table.Cell>{project.name}</Table.Cell>
+              <Table.Cell>{project.description}</Table.Cell>
+              <Table.Cell>
+                <Link href={project.image} target="_blank" className="emphasis">
+                  {project.image}
+                </Link>
+              </Table.Cell>
+              <Table.Cell>
+                <Link href={project.link} target="_blank" className="emphasis">
+                  {project.link}
+                </Link>
+              </Table.Cell>
+              <Table.Cell className="text-right">
+                {formatDate(project.updatedAt, {
+                  month: 'long',
+                  day: '2-digit',
+                  hour: '2-digit',
+                  minute: '2-digit',
+                })}
+              </Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table.Root>
+    )
+  },
+)
+ProjectsTable.displayName = 'ProjectsTable'
+
+export default ProjectsTable
--- src/app/[locale]/(authenticated)/projects/page.tsx
+++ src/app/[locale]/(authenticated)/projects/page.tsx
@@ -1,4 +1,7 @@
-import { CircleNotch } from '@phosphor-icons/react/dist/ssr'
+import ProjectsTable from './components/Table'
+
+import { TitleDesc } from '@/components'
+
 import { useTranslations } from 'next-intl'
 
 export default function Projects() {
@@ -6,12 +9,12 @@ export default function Projects() {
   const tSidebar = useTranslations('sidebar')
 
   return (
-    <main className="flex h-full w-full flex-col items-center justify-center gap-8">
-      <h1 className="text-5xl">{tSidebar('projects')}</h1>
-      <div className="flex animate-pulse items-center justify-center gap-4 text-center dark:text-gray-300">
-        <CircleNotch className="animate-spin" />
-        <span>{tHome('underDevelopment')}</span>
-      </div>
+    <main className="flex h-full w-full flex-col gap-8">
+      <TitleDesc
+        title={tSidebar('projects')}
+        description={tHome('underDevelopment')}
+      />
+      <ProjectsTable />
     </main>
   )
 }

eb0fd7076cb94f4ef95830a9a138effc5f93cf72
Yago Russo <yagrrusso@gmail.com>
2024-03-22 20:10:23 -0300

    chore: table changes

--- src/components/Table/Root.tsx
+++ src/components/Table/Root.tsx
@@ -1,17 +1,35 @@
+import { TableCaption } from './Caption'
+
+import { IconText } from '..'
+
 import { cn } from '@/lib/utils'
 
+import { CircleNotch } from '@phosphor-icons/react/dist/ssr'
 import { HTMLAttributes, forwardRef } from 'react'
 
-export interface TableRootProps extends HTMLAttributes<HTMLTableElement> {}
+export interface TableRootProps extends HTMLAttributes<HTMLTableElement> {
+  isLoading?: boolean
+  isEmpty?: boolean
+}
 
 export const TableRoot = forwardRef<HTMLTableElement, TableRootProps>(
-  ({ className, ...rest }, ref) => (
+  ({ isLoading, isEmpty, children, className, ...rest }, ref) => (
     <div className="relative w-full overflow-auto rounded">
       <table
         ref={ref}
         className={cn('w-full caption-bottom text-sm', className)}
         {...rest}
-      />
+      >
+        {children}
+        {(isEmpty || isLoading) && (
+          <TableCaption className="py-8">
+            <IconText
+              icon={isLoading && <CircleNotch className="animate-spin" />}
+              text={isLoading ? 'Loading...' : 'Empty'}
+            />
+          </TableCaption>
+        )}
+      </table>
     </div>
   ),
 )
--- src/components/Table/Row.tsx
+++ src/components/Table/Row.tsx
@@ -2,14 +2,17 @@ import { cn } from '@/lib/utils'
 
 import { HTMLAttributes, forwardRef } from 'react'
 
-export interface TableRowProps extends HTMLAttributes<HTMLTableRowElement> {}
+export interface TableRowProps extends HTMLAttributes<HTMLTableRowElement> {
+  hasHover?: boolean
+}
 
 export const TableRow = forwardRef<HTMLTableRowElement, TableRowProps>(
-  ({ className, ...rest }, ref) => (
+  ({ hasHover = true, className, ...rest }, ref) => (
     <tr
       ref={ref}
       className={cn(
-        'border-b border-gray-200 transition-colors hover:bg-gray-100/50 data-[state=selected]:bg-gray-100 dark:border-gray-900 dark:hover:bg-gray-800/50 dark:data-[state=selected]:bg-gray-800',
+        'border-b border-gray-200 transition-colors data-[state=selected]:bg-gray-100 dark:border-gray-900 dark:data-[state=selected]:bg-gray-800',
+        hasHover && 'hover:bg-gray-100/50 dark:hover:bg-gray-800/50',
         className,
       )}
       {...rest}

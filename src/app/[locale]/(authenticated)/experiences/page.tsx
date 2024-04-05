1e4984856b373659dd712f7fcde0af6d5cdb914c
Yago Russo <yagrrusso@gmail.com>
2024-04-04 23:23:35 -0300

    feat: experience form

--- src/app/[locale]/(authenticated)/experiences/page.tsx
+++ src/app/[locale]/(authenticated)/experiences/page.tsx
@@ -1,3 +1,4 @@
+import ExperiencesForm from './components/Form'
 import ExperiencesTable from './components/Table'
 
 import { TitleDesc } from '@/components'
@@ -11,11 +12,12 @@ export default function Experiences() {
 
   return (
     <main className="flex h-full w-full flex-col gap-8">
-      <div className="flex items-end justify-between">
+      <div className="flex justify-between gap-4 max-xs:flex-col">
         <TitleDesc
           title={tSidebar('experiences')}
           description={tHome('underDevelopment')}
         />
+        <ExperiencesForm />
       </div>
       <ExperiencesTable />
     </main>

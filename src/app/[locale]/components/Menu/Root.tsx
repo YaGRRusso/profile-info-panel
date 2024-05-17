2876db40788f03bf27cbb02824592ba432fc9b11
Yago Russo <yagrrusso@gmail.com>
2024-05-16 21:08:25 -0300

    chore: auto close sidebar on route change

--- src/app/[locale]/components/Menu/Root.tsx
+++ src/app/[locale]/components/Menu/Root.tsx
@@ -1,14 +1,21 @@
 'use client'
 
+import { usePathname } from '@/common/navigation'
+
 import { X } from '@phosphor-icons/react'
 import { List } from '@phosphor-icons/react/dist/ssr'
 import Image from 'next/image'
-import { FC, HTMLAttributes, useState } from 'react'
+import { FC, HTMLAttributes, useEffect, useState } from 'react'
 
 export interface MenuRootProps extends HTMLAttributes<HTMLDivElement> {}
 
 const MenuRoot: FC<MenuRootProps> = ({ children, ...rest }) => {
   const [isExpanded, setIsExpanded] = useState(false)
+  const pathname = usePathname()
+
+  useEffect(() => {
+    setIsExpanded(false)
+  }, [pathname])
 
   return (
     <>
@@ -16,7 +23,7 @@ const MenuRoot: FC<MenuRootProps> = ({ children, ...rest }) => {
         className="[&_*]:ellipsis sticky top-0 flex h-screen w-64 flex-col gap-2 border-r border-gray-900 bg-gray-900 p-4 text-gray-50 dark:bg-gray-950 max-lg:hidden"
         {...rest}
       >
-        <div className="mb-4 mt-8 flex items-center justify-center gap-2 border-b border-gray-800 px-2 pb-6 text-3xl font-medium dark:border-gray-900">
+        <div className="my-4 flex items-center justify-center gap-2 border-b border-gray-800 px-2 pb-6 text-3xl font-medium dark:border-gray-900">
           <Image src="/yr-branco.svg" alt="logo" width={24} height={24} />
           <span>
             <strong className="font-semibold text-sky-500">Yago</strong>Russo
@@ -43,7 +50,7 @@ const MenuRoot: FC<MenuRootProps> = ({ children, ...rest }) => {
       </header>
 
       <div
-        className="fixed inset-0 z-30 hidden translate-x-full flex-col bg-gray-900 text-gray-50 transition-all aria-[expanded='true']:translate-x-0 max-lg:flex"
+        className="fixed inset-0 z-30 hidden translate-x-full flex-col bg-gray-900 text-gray-50 transition-all aria-[expanded='true']:translate-x-0 dark:bg-gray-950 max-lg:flex"
         aria-expanded={isExpanded}
       >
         <div
